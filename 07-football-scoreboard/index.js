178aaba284595f1bc8b397f31942b395c1c15ca7
coen <22282057+Kewner@users.noreply.github.com>
2024-12-07 15:32:33 +0100

    Add main title, reset score button, max amount of goals alert. Improve goal visuals.

--- 07-football-scoreboard/index.js
+++ 07-football-scoreboard/index.js
@@ -1,30 +1,52 @@
-// Add 'New Game' button
-// Highlight the leader
 // Add more counters (period, fouls, timer)
-// Change the design
 
 let scoreHome = 0;
 let scoreGuest = 0;
 
-let goalsHome = document.getElementById('goals-home');
-let goalsGuest = document.getElementById('goals-guest');
+let goalsHome = document.getElementById('goal-home');
+let goalsGuest = document.getElementById('goal-guest');
 
 function homeGoal() {
     if (scoreHome === 18) {
-        console.log("No more room, you've scored too many goals!");
+        alert("Come on, who scores this many goals!?");
         return;
     }
     
     scoreHome += 1;
-    goalsHome.textContent += '⚽';
+    goalsHome.textContent = '⚽'.repeat(scoreHome);
+    highlightLeader();
 }
 
 function guestGoal() {
     if (scoreGuest === 18) {
-        console.log("No more room, you've scored too many goals!");
+        alert("Come on, who scores this many goals!?");
         return;
     }
 
     scoreGuest += 1;
-    goalsGuest.textContent += '⚽';
+    goalsGuest.textContent = '⚽'.repeat(scoreGuest);
+    highlightLeader();
+}
+
+let homeTeamTitle = document.getElementById('home-team-title');
+let guestTeamTitle = document.getElementById('guest-team-title');
+
+function highlightLeader() {
+    if (scoreHome > scoreGuest) {
+        homeTeamTitle.style.border = "6px solid gold";
+        guestTeamTitle.style.border = "6px solid transparent";
+    } else if (scoreGuest > scoreHome) {
+        guestTeamTitle.style.border = "6px solid gold";
+        homeTeamTitle.style.border = "6px solid transparent";
+
+    }
+}
+
+function resetScore() {
+    scoreHome = 0;
+    scoreGuest = 0;
+    goalsHome.textContent = '⚽'.repeat(scoreHome);
+    goalsGuest.textContent = '⚽'.repeat(scoreGuest);
+    guestTeamTitle.style.border = "6px solid transparent";
+    homeTeamTitle.style.border = "6px solid transparent";
 }
