50a97c953270b9cd2676cca3ba8169fc625ef5b2
coen <22282057+Kewner@users.noreply.github.com>
2024-12-17 10:49:52 +0100

    Add functionality for use numbers and use symbols toggle options

--- 09-password-generator-stretch-goals/index.js
+++ 09-password-generator-stretch-goals/index.js
@@ -1,20 +1,28 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
-let passwordOneEl = document.getElementById("password-one");
-let passwordTwoEl = document.getElementById("password-two");
-
-// let numbersInput = document.getElementById("toggle-numbers").value;
-
 function getPasswords() {
-    let lengthInput = Number(document.getElementById("length-input").value);
-    if (!validateLengthInput(lengthInput)) return;
+    let length = Number(document.getElementById("length-input").value);
+    if (!validateLengthInput(length)) return;
 
-    passwordOneEl.textContent = generatePassword(lengthInput);
-    passwordTwoEl.textContent = generatePassword(lengthInput);
+    let useNumbers = document.getElementById("toggle-numbers").checked;
+    let useSymbols = document.getElementById("toggle-symbols").checked;
+
+    let passwordOneEl = document.getElementById("password-one");
+    let passwordTwoEl = document.getElementById("password-two");
+    passwordOneEl.textContent = generatePassword(length, useNumbers, useSymbols);
+    passwordTwoEl.textContent = generatePassword(length, useNumbers, useSymbols);
 }
 
-function generatePassword(length) {
+function generatePassword(length, useNumbers, useSymbols) {
+    let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+    if (useNumbers && !useSymbols) {
+        characters = characters.slice(0, 62);
+    } else if (!useNumbers && useSymbols) {
+        characters = characters.slice(0, 52).concat(characters.slice(62, 91));
+    } else if (!useNumbers && !useSymbols) {
+        characters = characters.slice(0, 52);
+    }
+
     let password = '';
 
     for (let i = 0; i < length; i++) {
@@ -25,8 +33,8 @@ function generatePassword(length) {
     return password;
 }
 
-function validateLengthInput(lengthInput) {
-    if (lengthInput > 30 || lengthInput < 15) {
+function validateLengthInput(length) {
+    if (length > 30 || length < 15) {
         alert("Password length must be between 15 and 30!");
         return false;
     }
