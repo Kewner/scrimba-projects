6deada6c01f2d18555a39b7fb1a374851d7b9a69
coen <22282057+Kewner@users.noreply.github.com>
2024-12-18 18:13:18 +0100

    Replace onclick attribute of button with event listener

--- 09-password-generator-stretch-goals/index.js
+++ 09-password-generator-stretch-goals/index.js
@@ -1,4 +1,6 @@
-function getPasswords() {
+const btnPassword = document.getElementById("btn-password");
+
+btnPassword.addEventListener("click", function() {
     let length = Number(document.getElementById("length-input").value);
     if (!validateLengthInput(length)) return;
     document.getElementById("length-alert").style = "none";
@@ -10,7 +12,7 @@ function getPasswords() {
     let passwordTwoEl = document.getElementById("password-two");
     passwordOneEl.textContent = generatePassword(length, useNumbers, useSymbols);
     passwordTwoEl.textContent = generatePassword(length, useNumbers, useSymbols);
-}
+});
 
 function generatePassword(length, useNumbers, useSymbols) {
     let characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
